d9bc836d4bb82ee6d4199f7d0a58589c946b571f
Eric Mugo <mugoericpg20@gmail.com>
2023-05-22 15:16:42 +0300

    Added attributes

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
-
-
+class Book
+    attr_reader :title, :author, :page_count, :genre
+  
+    def initialize(title)
+      @title = title
+    end
+  
+    def author=(author)
+      @author = author
+    end
+  
+    def page_count=(page_count)
+      @page_count = page_count
+    end
+  
+    def genre=(genre)
+      @genre = genre
+    end
+  
+    
+  end
+  
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,30 @@
-# Make your shoe class here!
+class Shoe
+    attr_reader :brand, :color, :size, :material, :condition
+  
+    def initialize(brand)
+      @brand = brand
+      @condition = "new"
+    end
+  
+    def color=(color)
+      @color = color
+    end
+  
+    def size=(size)
+      @size = size
+    end
+  
+    def material=(material)
+      @material = material
+    end
+  
+    def condition=(condition)
+      @condition = condition
+    end
+  
+    def cobble
+      @condition = "new"
+      "Your shoe has been repaired."
+    end
+  end
+  
